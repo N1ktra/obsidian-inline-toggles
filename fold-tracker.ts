75ea3974aef74a63cac18de9669be7991335ac93
Niklas Tran <niklastran2010@yahoo.de>
2026-03-22 15:30:52 +0100

    FoldTracker wird jetzt korrekt gesperrt
    Bei einem "layout change" wird jetzt der last mode nochmal gesetzt

--- fold-tracker.ts
+++ fold-tracker.ts
@@ -4,91 +4,96 @@ import { Line } from "@codemirror/state";
 import { getToggleRegex } from "utils";
 import { MarkdownView } from "obsidian";
 
+
+export let foldTrackerSpec: any;
 export const createFoldTrackerPlugin = (plugin: any, settings: any) => {
-    return ViewPlugin.fromClass(class {
-        private lastMode: string = "";
-        private isSwitching: boolean = true;
-        private switchTimeout: number | null = null;
+    if (!foldTrackerSpec) {
+        foldTrackerSpec = ViewPlugin.fromClass(class {
+            public lastMode: string = "";
+            private isSwitching: boolean = true;
+            private switchTimeout: number | null = null;
 
-        constructor(readonly view: EditorView) {
-            // Initialen Modus setzen
-            const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
-            if (activeView) {
-                this.lastMode = activeView.getMode();
-            }
-        }
-
-        private triggerLock(duration: number = 300) {
-            this.isSwitching = true;
-            // Bestehenden Timer abbrechen (Reset)
-            if (this.switchTimeout) {
-                window.clearTimeout(this.switchTimeout);
-            }
-            // Neuen Timer starten
-            this.switchTimeout = window.setTimeout(() => {
-                this.isSwitching = false;
-                this.switchTimeout = null;
-                console.log("Fold-Tracker: Editor stabil, Sperre aufgehoben.");
-            }, duration);
-        }
-
-        update(update: ViewUpdate) {
-            const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
-            console.log(this.lastMode, activeView.getMode())
-            if (activeView) {
-                const currentMode = activeView.getMode();
-
-                // Bei JEDER Änderung des Modus (Reading <-> Editing)
-                if (this.lastMode !== currentMode) {
-                    this.lastMode = currentMode;
-                    this.triggerLock();
-                    return; // Erstes Wechsel-Event sofort blockieren
+            constructor(readonly view: EditorView) {
+                // Initialen Modus setzen
+                const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+                if (activeView) {
+                    this.lastMode = activeView.getMode();
                 }
             }
-            if (this.isSwitching) this.triggerLock();
 
+            private triggerLock(duration: number = 300) {
+                this.isSwitching = true;
+                // Bestehenden Timer abbrechen (Reset)
+                if (this.switchTimeout) {
+                    window.clearTimeout(this.switchTimeout);
+                }
+                // Neuen Timer starten
+                this.switchTimeout = window.setTimeout(() => {
+                    this.isSwitching = false;
+                    this.switchTimeout = null;
+                    // console.log("Fold-Tracker: Editor stabil, Sperre aufgehoben.");
+                }, duration);
+            }
 
-            for (let tr of update.transactions) {
-                for (let effect of tr.effects) {
-                    if (effect.is(foldEffect)) {
-                        const pos = effect.value.from;
-                        const line = update.state.doc.lineAt(pos);
-                        // console.log(`Sektion GEFALTET in Zeile: ${line.number}`);
-                        // console.log(`Inhalt der Kopfzeile: "${line.text}"`);
-                        this.updateToggle(update.view, line, false)
+            update(update: ViewUpdate) {
+                const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+                // console.log(this.lastMode, "->", activeView.getMode())
+                if (activeView) {
+                    const currentMode = activeView.getMode();
+                    // Bei JEDER Änderung des Modus (Reading <-> Editing)
+                    if (this.lastMode !== currentMode) {
+                        this.lastMode = currentMode;
+                        this.triggerLock();
+                        return; // Erstes Wechsel-Event sofort blockieren
                     }
-                    else if (effect.is(unfoldEffect)) {
-                        const pos = effect.value.from;
-                        const line = update.state.doc.lineAt(pos);
-                        // console.log(`Sektion AUFGEFALTET in Zeile: ${line.number}`);
-                        this.updateToggle(update.view, line, true)
+                }
+                if (this.isSwitching){
+                  this.triggerLock();
+                  return;
+                }
+
+
+                for (let tr of update.transactions) {
+                    for (let effect of tr.effects) {
+                        if (effect.is(foldEffect)) {
+                            const pos = effect.value.from;
+                            const line = update.state.doc.lineAt(pos);
+                            console.log(`Sektion GEFALTET in Zeile: ${line.number}`);
+                            this.updateToggle(update.view, line, false)
+                        }
+                        else if (effect.is(unfoldEffect)) {
+                            const pos = effect.value.from;
+                            const line = update.state.doc.lineAt(pos);
+                            console.log(`Sektion AUFGEFALTET in Zeile: ${line.number}`);
+                            this.updateToggle(update.view, line, true)
+                        }
                     }
                 }
             }
-        }
 
-        updateToggle(view: EditorView, line: Line, isOpen: boolean){
-            // console.log("updating toggle", isOpen)
-            const text = view.state.doc.sliceString(line.from, line.to);
-            const regex = getToggleRegex({textOpen: settings.placeholderOpen, textClosed: settings.placeholderClosed});
-            const match = regex.exec(text)
-            if(match)
-            {
-                const targetSymbol = isOpen ? settings.placeholderOpen : settings.placeholderClosed
-                if (match[0] === targetSymbol) return; // nichts machen falls symbol schon korrekt ist
-                const pos = line.from + match.index;
-                window.requestAnimationFrame(() => {
-                    view.dispatch({
-                        changes: {
-                            from: pos,
-                            to: pos + match[0].length,
-                            insert: targetSymbol
-                        },
-                        userEvent: "plugin.fold.symbol-update"
+            updateToggle(view: EditorView, line: Line, isOpen: boolean){
+                // console.log("updating toggle", isOpen)
+                const text = view.state.doc.sliceString(line.from, line.to);
+                const regex = getToggleRegex({textOpen: settings.placeholderOpen, textClosed: settings.placeholderClosed});
+                const match = regex.exec(text)
+                if(match)
+                {
+                    const targetSymbol = isOpen ? settings.placeholderOpen : settings.placeholderClosed
+                    if (match[0] === targetSymbol) return; // nichts machen falls symbol schon korrekt ist
+                    const pos = line.from + match.index;
+                    window.requestAnimationFrame(() => {
+                        view.dispatch({
+                            changes: {
+                                from: pos,
+                                to: pos + match[0].length,
+                                insert: targetSymbol
+                            },
+                            userEvent: "plugin.fold.symbol-update"
+                        });
                     });
-                });
+                }
             }
-
+        });
     }
-    });
+    return foldTrackerSpec;
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { Plugin } from 'obsidian';
+import { MarkdownView, Plugin } from 'obsidian';
 import { createToggleViewPlugin, createToggleEnterFix } from './view-plugin';
 import { insertOrRemoveToggle, scanAndApplyFold } from './logic';
-import { createFoldTrackerPlugin } from 'fold-tracker';
+import { createFoldTrackerPlugin, foldTrackerSpec } from 'fold-tracker';
 import { MyToggleSettings, DEFAULT_SETTINGS, MyToggleSettingTab } from './settings';
 
 export default class MyTogglePlugin extends Plugin {
@@ -11,11 +11,12 @@ export default class MyTogglePlugin extends Plugin {
         await this.loadSettings();
         this.addSettingTab(new MyToggleSettingTab(this.app, this));
 
+        createFoldTrackerPlugin(this, this.settings);
         // Editor Extension für die Icons
         this.registerEditorExtension([
             createToggleViewPlugin(this.settings),
             createToggleEnterFix(this.settings),
-            createFoldTrackerPlugin(this, this.settings)
+            foldTrackerSpec,
         ]);
 
         // Befehl zum Einfügen
@@ -40,6 +41,8 @@ export default class MyTogglePlugin extends Plugin {
         // Auto-Fold beim Tab-Wechsel
         this.registerEvent(
             this.app.workspace.on('layout-change', () => {
+                console.log("layout change")
+                this.setFoldTrackerLastMode();
                 scanAndApplyFold(this.app, this.settings);
             })
         );
@@ -51,5 +54,26 @@ export default class MyTogglePlugin extends Plugin {
 
     async saveSettings() {
         await this.saveData(this.settings);
+    }
+
+    /**
+     * Setzt die letzte gespeicherten Editor-View (preview / source)
+     */
+    private setFoldTrackerLastMode(){
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (!activeView) return;
+        const currentMode = activeView.getMode();
+        this.app.workspace.iterateAllLeaves((leaf) => {
+            if (leaf.view instanceof MarkdownView && leaf.view.editor) {
+                // @ts-ignore
+                const cm = leaf.view.editor.cm;
+                const trackerInstance = cm.plugin(foldTrackerSpec);
+
+                if (trackerInstance) {
+                    trackerInstance.lastMode = currentMode;
+                    console.log("Tracker lastMode manuell gesetzt auf:", trackerInstance.lastMode);
+                }
+            }
+        });
     }
 }
